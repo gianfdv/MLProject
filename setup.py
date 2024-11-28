f6200d28e2ea0e84fa47b56ed924793ed9871756
GianfrancoDV <gdarsie@hotmail.com>
2024-11-28 14:07:21 -0600

    setup.py and requirements

---
+++ setup.py
@@ -0,0 +1,29 @@
+## RESPONSIBLE IN CREATING MY MACHINE LEARNING APPLICATION AS A PACKAGE FOR LATER/COMMON USAGE
+
+from setuptools import find_packages,setup
+from typing import List
+
+hypen_e_dot = '-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+
+        if hypen_e_dot in requirements:
+            requirements.remove(hypen_e_dot)
+    
+    return requirements
+
+setup (
+    name = 'MLProject',
+    version = '0.0.1',
+    author = 'Gianfranco',
+    author_email = 'gdarsie@hotmail.com',
+    packages = find_packages(),
+    install_requires = get_requirements('requirements.txt')
+)
